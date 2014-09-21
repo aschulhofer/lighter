a1c3fb01d7d12abb3b7acd943f6668601a98c399
Doris Pedratscher <doris.pedratscher@gmail.com>
2014-09-21 17:30:45 +0200

    add Userstorytable

---
+++ Lighter/app/src/main/java/at/woodstick/lighter/db/tables/UserStoryTable.java
@@ -0,0 +1,28 @@
+package at.woodstick.lighter.db.tables;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+public class UserStoryTable implements BaseColumns {
+
+	public static final String TABLE_NAME = "user_story";
+	
+	private static final String CREATE_TABLE_QUERY = 
+		"CREATE TABLE " + TABLE_NAME + " ("
+		+ _ID + " INTEGER PRIMARY KEY"
+		+ ", user_story_title TEXT NOT NULL"
+		+ ", user_story_created_at INTEGER NOT NULL"
+		+ ")";
+	
+	private static final String DROP_TABLE_QUERY = "DROP TABLE IF EXISTS " + TABLE_NAME;
+	
+	public static void onCreate(SQLiteDatabase database) {
+		database.execSQL(CREATE_TABLE_QUERY);
+	}
+	
+	public static void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+		database.execSQL(DROP_TABLE_QUERY);
+		onCreate(database);
+		
+	}
+}
